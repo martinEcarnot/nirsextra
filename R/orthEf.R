b913e7af83e04f6805ebc106cd4a8e21eceef379
Martin Ecarnot <martin.ecarnot@inra.fr>
2020-09-15 14:16:12 +0200

    nettoyage orthEf

--- R/orthEf.R
+++ R/orthEf.R
@@ -1,15 +1,12 @@
 orthEf <- function (datref,datappli,spname,coleffet,ncomp) {
-  
-library(FactoMineR)
-library(rnirs)
-  
+
 # ieffet=which(colnames(datref) %in% effet)
 S=datref[,which(colnames(datref) %in% spname)]
 
   Ds=tab.disjonctif(datref[,coleffet])
   # browser()
   BS =  Ds %*% pinv(t(Ds) %*% Ds)$Xplus %*% t(Ds) %*% S
-  WS = S - BS; 
+  WS = S - BS;
 
 # ACP
   rpca=pca(WS,ncomp= ncomp)  # rpca=pca(WS,ncomp= qr(WS)$rank)
