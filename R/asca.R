3e40926adc065b49b11fd4649afd8d4ef572ca22
Martin Ecarnot <martin.ecarnot@inrae.fr>
2021-08-20 14:44:30 +0200

    modif onefitcv

--- R/asca.R
+++ R/asca.R
@@ -365,7 +365,8 @@ desorder=ldmatrices$desorder
 Xd<-Xp
 
 for ( i in 1 : length(dmatrices)) {
-  Xeff<-dmatrices[[i]] %*% ginv(dmatrices[[i]]) %*%Xd
+  print(i)
+  Xeff<-dmatrices[[i]] %*% ginv(dmatrices[[i]]) %*% Xd
   ssqEff<-sum(sum(Xeff^2))
     Xd<-Xd-Xeff
     l<-deslabels[[i]]
