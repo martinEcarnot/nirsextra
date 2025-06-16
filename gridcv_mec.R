2c2fa2ded8617d6dcc0f1355acef672ba95fb149
martinEcarnot <martin.ecarnot@inrae.fr>
2025-06-16 17:38:46 +0200

    +

---
+++ gridcv_mec.R
@@ -0,0 +1,160 @@
+gcv <- function(X, Y, segm, score, fun, pars, verb = TRUE) {
+  ## pars = List of named vectors (arguments) involved in the calculation of the score
+  Y <- .mat(Y, "y")
+  nrep <- length(segm)
+  res_rep <- vector("list", length = nrep)
+  nco <- length(pars[[1]])
+  for(i in seq_len(nrep)) {
+    if(verb)
+      cat("/ rep=", i, " ", sep = "")   
+    listsegm <- segm[[i]]
+    nsegm <- length(listsegm)
+    zres <- vector("list", length = nsegm)
+    for(j in seq_len(nsegm)) {
+      if(verb)
+        cat("segm=", j, " ", sep = "")
+      s <- sort(listsegm[[j]])
+      if((is.list(X)==TRUE)&(is.matrix(X)==FALSE)&(is.data.frame(X)==FALSE)){
+        zres[[j]] <- gridscore(
+          lapply(1:length(X), function(k) X[[k]][-s, , drop = FALSE]), 
+          Y[-s, , drop = FALSE],
+          lapply(1:length(X), function(k) X[[k]][s, , drop = FALSE]), 
+          Y[s, , drop = FALSE],
+          score = score, fun = fun, pars = pars)
+      }else{
+        zres[[j]] <- gridscore(
+          X[-s, , drop = FALSE], Y[-s, , drop = FALSE],
+          X[s, , drop = FALSE], Y[s, , drop = FALSE],
+          score = score, fun = fun, pars = pars)
+      }
+    }
+    zres <- setDF(rbindlist(zres))
+    res_rep[[i]] <- cbind(rep = rep(i, nsegm * nco),
+                          segm = sort(rep(1:nsegm, nco)), zres)
+  }
+  res_rep <- setDF(rbindlist(res_rep))
+  if(verb)
+    cat("/ End. \n\n")    
+  namy <- colnames(Y)
+  nampar <- names(pars) 
+  res <- aggregate(res_rep[, namy, drop = FALSE], 
+                   by = res_rep[, nampar, drop = FALSE], FUN = mean)
+  list(val = res, val_rep = res_rep)
+}
+
+gcvlv <- function(X, Y, segm, score, fun, nlv, pars = NULL, verb = TRUE) {
+  ## pars must not contains nlv
+  Y <- .mat(Y, "y")
+  nrep <- length(segm)
+  res_rep <- vector("list", length = nrep)
+  nlv <- seq(min(nlv), max(nlv))
+  le_nlv <- length(nlv) 
+  for(i in seq_len(nrep)) {
+    if(verb)
+      cat("/ rep=", i, " ", sep = "")     
+    listsegm <- segm[[i]]
+    nsegm <- length(listsegm)
+    zres <- vector("list", length = nsegm)
+    for(j in seq_len(nsegm)) {
+      if(verb)
+        cat("segm=", j, " ", sep = "")
+      s <- sort(listsegm[[j]])
+      if((is.list(X)==TRUE)&(is.matrix(X)==FALSE)&(is.data.frame(X)==FALSE)){
+        zres[[j]] <- gscorelv_mec(
+          lapply(1:length(X), function(k) X[[k]][-s, , drop = FALSE]),
+          Y[-s, , drop = FALSE],
+          lapply(1:length(X), function(k) X[[k]][s, , drop = FALSE]), 
+          Y[s, , drop = FALSE],
+          score = score, fun = fun, nlv = nlv, pars = pars)
+      }else{
+        zres[[j]] <- gscorelv_mec(
+          X[-s, , drop = FALSE], Y[-s, , drop = FALSE],
+          X[s, , drop = FALSE], Y[s, , drop = FALSE],
+          score = score, fun = fun, nlv = nlv, pars = pars)
+      }
+    }
+    
+    yp_list <- lapply(zres, function(x) x$yp)
+    ypred <- do.call(rbind, yp_list)
+    zres <- lapply(zres, function(x) {
+      x$yp <- NULL
+      return(x)
+    })
+  
+    zres <- setDF(rbindlist(zres))
+    ## Case where pars is empty
+    if(is.null(pars)) {
+      res_rep[[i]] <- cbind(rep = rep(i, nsegm * le_nlv),
+                            segm = sort(rep(1:nsegm, le_nlv)), zres)
+    }
+    ## End
+    else {
+      nco <- length(pars[[1]])
+      res_rep[[i]] <- cbind(rep = rep(i, nsegm * le_nlv * nco),
+                            segm = sort(rep(1:nsegm, le_nlv * nco)), zres)
+    }
+  }
+  res_rep <- setDF(rbindlist(res_rep))
+  if(verb)
+    cat("/ End. \n\n")        
+  namy <- colnames(Y)
+  nampar <- c("nlv", names(pars)) 
+  res <- aggregate(res_rep[, namy, drop = FALSE], 
+                   by = res_rep[, nampar, drop = FALSE], FUN = mean)
+  list(val = res, val_rep = res_rep, ypred=ypred)
+}
+
+gridcvlb <- function(X, Y, segm, score, fun, lb, pars = NULL, verb = TRUE) {
+  ## pars = List of named vectors (arguments) involved in the calculation of the score
+  ## Must not contains lb
+  Y <- .mat(Y, "y")
+  nrep <- length(segm)
+  res_rep <- vector("list", length = nrep)
+  lb <- sort(unique(lb))
+  le_lb <- length(lb) 
+  for(i in seq_len(nrep)) {
+    if(verb)
+      cat("/ rep=", i, " ", sep = "")     
+    listsegm <- segm[[i]]
+    nsegm <- length(listsegm)
+    zres <- vector("list", length = nsegm)
+    for(j in seq_len(nsegm)) {
+      if(verb)
+        cat("segm=", j, " ", sep = "")
+      s <- sort(listsegm[[j]])
+      if((is.list(X)==TRUE)&(is.matrix(X)==FALSE)&(is.data.frame(X)==FALSE)){
+        zres[[j]] <- gridscorelb(
+          lapply(1:length(X), function(k) X[[k]][-s, , drop = FALSE]), 
+          Y[-s, , drop = FALSE],
+          lapply(1:length(X), function(k) X[[k]][s, , drop = FALSE]), 
+          Y[s, , drop = FALSE],
+          score = score, fun = fun, lb = lb, pars = pars)
+      }else{
+        zres[[j]] <- gridscorelb(
+          X[-s, , drop = FALSE], Y[-s, , drop = FALSE],
+          X[s, , drop = FALSE], Y[s, , drop = FALSE],
+          score = score, fun = fun, lb = lb, pars = pars)
+      }
+    }
+    zres <- setDF(rbindlist(zres))
+    ## Case where pars is empty
+    if(is.null(pars)) {
+      res_rep[[i]] <- cbind(rep = rep(i, nsegm * le_lb),
+                            segm = sort(rep(1:nsegm, le_lb)), zres)
+    }
+    ## End
+    else {
+      nco <- length(pars[[1]])
+      res_rep[[i]] <- cbind(rep = rep(i, nsegm * le_lb * nco),
+                            segm = sort(rep(1:nsegm, le_lb * nco)), zres)
+    }
+  }
+  res_rep <- setDF(rbindlist(res_rep))
+  if(verb)
+    cat("/ End. \n\n")        
+  namy <- colnames(Y)
+  nampar <- c("lb", names(pars)) 
+  res <- aggregate(res_rep[, namy, drop = FALSE], 
+                   by = res_rep[, nampar, drop = FALSE], FUN = mean)
+  list(val = res, val_rep = res_rep)
+}
---
+++ gridscore_mec.R
@@ -0,0 +1,176 @@
+
+gscore_mec <- function(Xtrain, Ytrain, X, Y, score, fun, pars, verb = FALSE) {
+  ## pars = List of named vectors (arguments) involved in the calculation of the score
+  if((is.list(Xtrain)==TRUE)&(is.matrix(Xtrain)==FALSE)&(is.data.frame(Xtrain)==FALSE)){
+    Xtrain <- lapply(1:length(Xtrain), function(i) .mat(Xtrain[[i]]))
+  }else{
+    Xtrain <- .mat(Xtrain)
+  }
+  Ytrain <- .mat(Ytrain, "y") 
+  if((is.list(X)==TRUE)&(is.matrix(X)==FALSE)&(is.data.frame(X)==FALSE)){
+    X <- lapply(1:length(X), function(i) .mat(X[[i]]))
+  }else{
+    X <- .mat(X)
+  }
+  Y <- .mat(Y, "y")
+  q <- dim(Ytrain)[2]
+  nco <- length(pars[[1]])
+  npar <- length(pars)
+  if(verb) 
+    cat("-- Nb. combinations = ", nco, "\n")
+  res <- matrix(nrow = nco, ncol = q)
+  for(i in seq_len(nco)){
+    zpars <- lapply(pars, FUN = function(x) x[[i]])
+    if (verb)
+      print(data.frame(zpars))
+    fm <- do.call(
+      fun, 
+      c(list(Xtrain, Ytrain), zpars))
+    pred <- predict(fm, X)$pred
+    res[i, ] <- score(pred, Y)
+  }
+  if (verb) 
+    cat("-- End. \n\n")
+  colnames(res) <- colnames(Ytrain)
+  # res <- data.frame(pars, res, stringsAsFactors = FALSE)
+  res <- data.frame(pars, res, stringsAsFactors = FALSE)
+}
+
+gscorelv_mec <- function(Xtrain, Ytrain, X, Y, score, fun, nlv, pars = NULL, verb = FALSE) {
+  ## pars = List of named vectors (arguments) involved in the calculation of the score
+  ## Must not contains nlv
+  if((is.list(Xtrain)==TRUE)&(is.matrix(Xtrain)==FALSE)&(is.data.frame(Xtrain)==FALSE)){
+    Xtrain <- lapply(1:length(Xtrain), function(i) .mat(Xtrain[[i]]))
+  }else{
+    Xtrain <- .mat(Xtrain)
+  }
+  Ytrain <- .mat(Ytrain, "y")     
+  if((is.list(X)==TRUE)&(is.matrix(X)==FALSE)&(is.data.frame(X)==FALSE)){
+    X <- lapply(1:length(X), function(i) .mat(X[[i]]))
+  }else{
+    X <- .mat(X)
+  }
+  Y <- .mat(Y, "y")
+  n <- dim(Y)[1]
+  q <- dim(Ytrain)[2]
+  nlv <- seq(min(nlv), max(nlv))
+  le_nlv <- length(nlv) 
+  ypred = data.frame()
+  ## Case where pars is NULL
+  if(is.null(pars)) {
+    if(verb) 
+      cat("-- Nb. combinations = 0 \n")
+    fm <- fun(Xtrain, Ytrain, nlv = max(nlv))
+    pred <- predict(fm, X, nlv = nlv)$pred
+    if(le_nlv == 1)
+      pred <- list(pred)
+    res <- matrix(nrow = le_nlv, ncol = q)
+    for(i in seq_len(le_nlv)) {
+      res[i, ] <- score(pred[[i]], Y)
+      ypred=rbind(ypred,cbind(rep(i,n),rownames(Y),pred[[i]]))
+    }
+    colnames(res) <- colnames(Ytrain)
+    colnames(ypred) = c("nlv","rowname","y1")
+    res <- data.frame(nlv = nlv, res, stringsAsFactors = FALSE)
+    res2 <- list(res=res, yp=ypred) 
+    return(res2)
+  } 
+  ## End
+  else {
+    npar <- length(pars)
+    nco <- length(pars[[1]])
+    if(verb) 
+      cat("-- Nb. combinations = ", nco, "\n")
+    res <- vector(mode = "list", length = nco)
+    for(i in seq_len(nco)) {
+      zpars <- lapply(pars, FUN = function(x) x[[i]])
+      if (verb)
+        print(data.frame(zpars))
+      fm <- do.call(
+        fun,
+        c(list(Xtrain, Ytrain), nlv = max(nlv), zpars))
+      zpred <- predict(fm, X, nlv = nlv)$pred
+      if(le_nlv == 1)
+        zpred <- list(zpred)
+      zres <- matrix(nrow = le_nlv, ncol = q)
+      for(j in seq_len(le_nlv)) {
+        zres[j, ] <- score(zpred[[j]], Y)
+        ypred=rbind(ypred,cbind(rep(j,n),rownames(Y),rep(pars[[1]][i],n),zpred[[i]]))
+      }
+      colnames(zres) <- colnames(Ytrain)
+      head(ypred)
+      zres <- data.frame(nlv = nlv, zres, stringsAsFactors = FALSE)
+      res[[i]] <- suppressWarnings(data.frame(zpars, zres)) 
+    }
+    colnames(ypred) = c("nlv","rowname","pars",colnames(Ytrain))
+    res2 <- list(res=setDF(rbindlist(res)), yp=ypred)  
+    return(res2)
+  }
+  if (verb) 
+    cat("-- End. \n\n")
+  res
+}
+
+gridscorelb <- function(Xtrain, Ytrain, X, Y, score, fun, lb, pars = NULL, verb = FALSE) {
+  ## pars = List of named vectors (arguments) involved in the calculation of the score
+  ## Must not contains lb
+  if((is.list(Xtrain)==TRUE)&(is.matrix(Xtrain)==FALSE)&(is.data.frame(Xtrain)==FALSE)){
+    Xtrain <- lapply(1:length(Xtrain), function(i) .mat(Xtrain[[i]]))
+  }else{
+    Xtrain <- .mat(Xtrain)
+  }
+  Ytrain <- .mat(Ytrain, "y")     
+  if((is.list(X)==TRUE)&(is.matrix(X)==FALSE)&(is.data.frame(X)==FALSE)){
+    X <- lapply(1:length(X), function(i) .mat(X[[i]]))
+  }else{
+    X <- .mat(X)
+  }
+  Y <- .mat(Y, "y")
+  q <- dim(Ytrain)[2]
+  lb <- sort(unique(lb))
+  le_lb <- length(lb) 
+  ## Case where pars is empty
+  if(is.null(pars)) {
+    if(verb) 
+      cat("-- Nb. combinations = 0 \n")
+    fm <- fun(Xtrain, Ytrain, lb = max(lb))
+    pred <- predict(fm, X, lb = lb)$pred
+    if(le_lb == 1)
+      pred <- list(pred)
+    res <- matrix(nrow = le_lb, ncol = q)
+    for(i in seq_len(le_lb))
+      res[i, ] <- score(pred[[i]], Y)
+    colnames(res) <- colnames(Ytrain)
+    res <- data.frame(lb = lb, res, stringsAsFactors = FALSE)
+  } 
+  ## End
+  else {
+    npar <- length(pars)
+    nco <- length(pars[[1]])
+    if(verb) 
+      cat("-- Nb. combinations = ", nco, "\n")
+    res <- vector(mode = "list", length = nco)
+    for(i in seq_len(nco)) {
+      zpars <- lapply(pars, FUN = function(x) x[[i]])
+      if(verb) 
+        print(data.frame(zpars))
+      fm <- do.call(
+        fun,
+        c(list(Xtrain, Ytrain), lb = max(lb), zpars)
+      )
+      zpred <- predict(fm, X, lb = lb)$pred
+      if(le_lb == 1)
+        zpred <- list(zpred)
+      zres <- matrix(nrow = le_lb, ncol = q)
+      for(j in seq_len(le_lb))
+        zres[j, ] <- score(zpred[[j]], Y)
+      colnames(zres) <- colnames(Ytrain)
+      zres <- data.frame(lb = lb, zres, stringsAsFactors = FALSE)
+      res[[i]] <- suppressWarnings(data.frame(zpars, zres)) 
+    }
+    res <- setDF(rbindlist(res))    
+  }
+  if (verb) 
+    cat("-- End. \n\n")
+  res
+}
