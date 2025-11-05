8ef3ec2497a20bdda0599f68f083cce8dfea7d11
martinEcarnot <martin.ecarnot@inrae.fr>
2025-11-05 14:09:06 +0100

    add naturaspec2df

---
+++ R/naturaspec2df.R
@@ -0,0 +1,13 @@
+#' naturaspec2df
+#'
+#' Read NaturaSpec files and set it to a dataframe
+#'
+#' @export
+#'
+naturaspec2df <- function(d) {
+  sp=read_spectra(d)
+  x=sp$value
+  colnames(x)=sp$bands
+  row.names(x)=substr(sp$names,1,nchar(sp$names)-9)
+  df=sp2df(x)
+}
