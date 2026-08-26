40e382e381e81d503d93c16b70ddffe9420c4e8f
martinEcarnot <martin.ecarnot@inrae.fr>
2026-08-26 11:08:49 +0200

    +

--- R/adj_NaturaSpec.R
+++ R/adj_NaturaSpec.R
@@ -17,7 +17,7 @@ adj_NaturaSpec <- function(X) {
   for (iadj in c(632)) {
 
     x= (iadj - ws + 1):iadj
-    Y=Xi[, x]
+    Y=X[, x]
     my=t(colMeans(t(Y)))
 
     sx=var(x)
@@ -26,13 +26,13 @@ adj_NaturaSpec <- function(X) {
     b0=my-b*mx  # Ajustement linaire.
 
     # 14 points after 632 seems to be interpolated: replaced by linear adjustment from 5 last points
-    Xi[,(iadj+1):(iadj+interpw)] = drop(replicate(interpw,b0)) + t(b)%*%((iadj+1):(iadj+interpw))
+    X[,(iadj+1):(iadj+interpw)] = drop(replicate(interpw,b0)) + t(b)%*%((iadj+1):(iadj+interpw))
 
     # Compute difference between adjusted and original spectra after gap, and apply it to the rest of the spectrum
-    dif=Xi[,iadj+interpw+1]-(b0+b*(iadj+interpw+1))
-    loremp=(iadj+interpw+1):dim(Xi)[2]
-    Xi[,loremp]=Xi[,loremp] - drop(replicate(length(loremp),dif))
+    dif=X[,iadj+interpw+1]-(b0+b*(iadj+interpw+1))
+    loremp=(iadj+interpw+1):dim(X)[2]
+    X[,loremp]=X[,loremp] - drop(replicate(length(loremp),dif))
   }
-  return(Xi)
+  return(X)
 
 }
